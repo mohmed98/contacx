760bbc05b32e77cc4fecc9c3c4c767090f73a107
mohmed98 <mohamedatta447@gmail.com>
2021-07-23 00:45:32 +0200

    displayed user avatar

--- src/parts/Nav.js
+++ src/parts/Nav.js
@@ -1,34 +1,29 @@
-import { Link } from "react-router-dom"
-const axios = require('axios')
+import { useState, useEffect } from "react";
+
 export default function Nav() {
-    async function getMainUser() {
-        try {
-            const response = await axios.get('http://localhost:3000/poeple?id=1');
-            console.log(response.data[0]);
-            return response.data[0];
-        } catch (error) {
-            console.error(error);
-        }
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/poeple?id=1")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItem(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return item.map((i) => <img className='user-avatar' src={i.avatar} />);
     }
-    const mainUser = getMainUser()
-    return (
-        <div>
-            {mainUser}
-            {/* <img src={mainUser.avatar} /> */}
-            {/* <nav>
-                <ul>
-                    <li>
-                        <Link to="/"> Contacts </Link>
-                    </li>
-                    <li>
-                        <Link to="/single"> About </Link>
-                    </li>
-                    <li>
-                        <Link to="/users"> Users </Link>
-                    </li>
-                </ul>
-            </nav>
-             */}
-        </div>
-    )
 }
