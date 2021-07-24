fb42a9be791e383beceb9e00e674efdfdd7839c7
mohmed98 <mohamedatta447@gmail.com>
2021-07-24 23:32:24 +0200

    displyed home content

--- src/parts/ContactList.js
+++ src/parts/ContactList.js
@@ -1,6 +1,21 @@
 
+import useFetch from "../useFetch";
+
 export default function ContentList() {
+    const { error, isPending, data: conatacts } = useFetch('http://localhost:3000/poeple')
+    console.log(conatacts);
     return (
-        <div>contnt list</div>
+        <div>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {conatacts && (
+
+                conatacts.map(contact => (
+                    <div key={contact.id}>{contact.name}</div>
+
+                ))
+
+            )}
+        </div>
     )
 }
